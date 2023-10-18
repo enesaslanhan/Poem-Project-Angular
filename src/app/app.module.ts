36b817ea9403d16e4ede852b63e4af5bd3c2f502
enesaslanhan <you@example.com>
2023-10-18 11:01:55 +0300

    18 Ekim 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NaviComponent } from './Components/navi/navi.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NaviComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
