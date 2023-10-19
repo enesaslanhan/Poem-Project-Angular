9a6c2ac3034775a42d001d09f49e9cc31e8f7538
enesaslanhan <you@example.com>
2023-10-19 07:57:49 +0300

    19 Ekim 1

--- src/app/Components/navi/navi.component.ts
+++ src/app/Components/navi/navi.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navi',
   templateUrl: './navi.component.html',
   styleUrls: ['./navi.component.css']
 })
-export class NaviComponent {
+export class NaviComponent implements OnInit{
+
+  ngOnInit(): void {
+    
+  }
+  
+  toggleOpen(){
+    const toggleBtn=document.querySelector('.toggle_btn')
+    const toggleBtnIcon=document.querySelector('.toggle_btn i')
+    const dropDownMenu=document.querySelector('.dropdown_menu')
+    dropDownMenu.classList.toggle('open')
+    const isOpen=dropDownMenu.classList.contains('open')
+    if (isOpen) {
+      toggleBtnIcon.classList.add('fa-xmark');
+      toggleBtnIcon.classList.remove('fa-bars');
+    } else {
+      toggleBtnIcon.classList.add('fa-bars');
+      toggleBtnIcon.classList.remove('fa-xmark');
+    }
+    
+  }
 
 }
